1bad6c6329085df4875f7c6c69f5c2f0356c2d6c
Волков Вадим <volkovva.pir231@gamil.com>
2025-04-27 16:18:42 +0300

    feat: add homepage.js file, swiper.js file, add link for homepage.js for slider to work

---
+++ js/homepage/homepage.js
@@ -0,0 +1,5 @@
+import { initCommentsSwiper } from './swiper.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    initCommentsSwiper();
+});
---
+++ js/homepage/swiper.js
@@ -0,0 +1,15 @@
+export function initCommentsSwiper() {
+    const swiper = new Swiper('.comments-slider', {
+        slidesPerView: 1,
+        spaceBetween: 36,
+        loop: false,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+}
