198aff2a53fa245684568558359ae8b1f4540dba
Волков Вадим <volkovva.pir231@gamil.com>
2025-04-30 10:00:59 +0300

    feat: add functionality to add service to cart

--- js/service/service.js
+++ js/service/service.js
@@ -12,3 +12,26 @@ fetch(`http://localhost:3000/services/${id}`)
     document.querySelector('.service-img').src = service.image;
   })
   .catch(err => console.error('Ошибка при получении данных:', err));
+
+addToCart();
+
+async function addToCart(params) {
+  const userId = 1;
+  const res = await fetch('http://localhost:3000/users/1');
+  const user = await res.json();
+  const serviceId = parseInt(new URLSearchParams(window.location.search).get('id'));
+
+  const cartButton = document.querySelector('.card-button:last-child');
+  cartButton.addEventListener('click', async () => {
+    if (!user.cart.includes(serviceId)) {
+      user.cart.push(serviceId);
+    }
+
+    await fetch(`http://localhost:3000/users/${userId}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ cart: user.cart })
+    });
+  });
+}
+  
