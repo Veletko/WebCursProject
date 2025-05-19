442c8d1ae1abc432f8c156cb61dc4d1050f5014c
Волков Вадим <volkovva.pir231@gamil.com>
2025-05-19 16:12:19 +0300

    feat: add mobileMenu.js file with burger menu

---
+++ js/baseElements/mobileMenu.js
@@ -0,0 +1,36 @@
+export function initMobileMenu(placeholderId = 'mobile-menu-placeholder') {
+    const placeholder = document.getElementById(placeholderId);
+    if (!placeholder) {
+        console.error(`Mobile menu placeholder with ID "${placeholderId}" not found.`);
+        return;
+    }
+
+    placeholder.innerHTML = `
+        <div class="mobile-menu" id="mobile-menu">
+            <span class="mobile-menu-close">&times;</span>
+            <nav class="mobile-nav">
+                <a href="/pages/login.html">Log in</a>
+                <a href="/pages/homepage.html">Home</a>
+                <a href="/pages/services.html">Services</a>
+            </nav>
+            <div class="mobile-menu-footer">
+            <button class="visually-impaired-button">
+                <img src="/img/eye.png" alt="eye">
+                version for the visually impaired
+            </button>
+                <label class="theme-toggle">
+                    <input type="checkbox" hidden>  
+                    <img class="sun" src="/img/sun.png" alt="sun">
+                    <img class="moon" src="/img/moon.png" alt="moon">
+                </label>
+                <div class="mobile-language-selector">
+                    <input type="checkbox" id="mobile-lang-switch" class="language-checkbox" hidden>
+                    <label for="mobile-lang-switch" class="language-toggle">
+                      <span class="language-option ru">RU</span>
+                      <span class="language-option en">EN</span>
+                    </label>
+                </div>
+            </div>
+        </div>
+    `;
+}
