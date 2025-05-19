0a209a46991315d5f766186972d49d469399a753
Волков Вадим <volkovva.pir231@gamil.com>
2025-05-19 16:28:03 +0300

    fix: change theme.js file for it to work with burger menu

--- js/baseElements/theme.js
+++ js/baseElements/theme.js
@@ -1,25 +1,26 @@
 export function initThemeToggle() {
-    const themeCheckbox = document.querySelector(".theme-toggle input");
+    const themeCheckboxes = document.querySelectorAll(".theme-toggle input");
     const htmlElement = document.documentElement;
 
     function setTheme(theme) {
         htmlElement.setAttribute("data-theme", theme);
         localStorage.setItem("theme", theme);
+        
+        themeCheckboxes.forEach(checkbox => {
+            checkbox.checked = (theme === "dark");
+        });
     }
 
     function loadTheme() {
-        const savedTheme = localStorage.getItem("theme");
+        const savedTheme = localStorage.getItem("theme") || "light";
         setTheme(savedTheme);
-        if (themeCheckbox) {
-            themeCheckbox.checked = (savedTheme === "dark");
-        }
     }
 
-    if (themeCheckbox) {
-        themeCheckbox.addEventListener("change", function() {
+    themeCheckboxes.forEach(checkbox => {
+        checkbox.addEventListener("change", function() {
             setTheme(this.checked ? "dark" : "light");
         });
-    }
+    });
 
     loadTheme();
 }
