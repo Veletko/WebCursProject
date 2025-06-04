749ebd1f83d3247c20d63d0d9cf985fdf48b3cca
Волков Вадим <volkovva.pir231@gamil.com>
2025-06-04 23:00:21 +0300

    fix: update loginCheck.js file

--- js/baseElements/loginCheck.js
+++ js/baseElements/loginCheck.js
@@ -1,19 +1,4 @@
 export function checkAuth() {
     const currentUser = JSON.parse(localStorage.getItem('currentUser'));
-    const basketLink = document.querySelector('.basket');
-
-    if (basketLink) {
-
-        basketLink.replaceWith(basketLink.cloneNode(true));
-        
-        if (!currentUser) {
-            basketLink.addEventListener('click', (e) => {
-                e.preventDefault();
-                alert('Please register or log in to use the cart.');
-            });
-        } else {
-            basketLink.href = 'cart.html';
-        }
-    }
     return currentUser;
 }
